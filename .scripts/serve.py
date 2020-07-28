134ed6845657bb49c8a877decc86b6932f8bdf41
Steve Dower <steve.dower@microsoft.com>
2020-07-28 20:14:35 +0100

    Add functions.html route to local test

--- .scripts/serve.py
+++ .scripts/serve.py
@@ -11,11 +11,37 @@ sys.path.insert(0, str(ROOT))
 import Functions
 from handlers import execute_function, get_all_metadata
 
+PAGE = """<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Custom Functions</title>
+    <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/9.6.1/css/fabric.min.css">
+    <script src="https://exceljupyter.azurewebsites.net/agave/external/react-16-12-0/umd/react.development.js"></script>
+    <script src="https://exceljupyter.azurewebsites.net/agave/external/react-dom-16-12-0/umd/react-dom.development.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.5/require.min.js"></script>
+    <script type="text/javascript" src="https://appsforoffice.microsoft.com/lib/1.1/hosted/office.debug.js"></script>
+    <script type="text/javascript" src="https://exceljupyter.azurewebsites.net/agave/custom-function-forwarder.bundle.js"></script>
+</head>
+    <body>
+        <div id="DivApp">
+        </div>
+        <div id="DivLog">
+        </div>
+    </body>
+</html>""".encode()
+
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/functions":
             self._send_json(get_all_metadata(Functions))
+        elif self.path == "/functions.html":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html;charset=utf-8")
+            self.send_header("Content-Length", str(len(PAGE)))
+            self.end_headers()
+            self.wfile.write(PAGE)
         else:
             self.send_error(404)
 
