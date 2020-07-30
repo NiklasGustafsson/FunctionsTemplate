fa77e307d164ee3d3efdc8de3a870f5e22bf5ee1
Steve Dower <steve.dower@microsoft.com>
2020-07-30 20:11:54 +0100

    F strings

--- Functions.py
+++ Functions.py
@@ -5,12 +5,12 @@ import pandas
 from urllib.request import urlopen
 
 def countrydata(country : str) -> Matrix:
-    return pandas.read_csv("https://excelpythonbase.azurewebsites.net/covidata?country={country}")
+    return pandas.read_csv(f"https://excelpythonbase.azurewebsites.net/covidata?country={country}")
 
 
 def countryplot(country : str):
     """Return an image"""
-    with urlopen("https://excelpythonbase.azurewebsites.net/covidata?country={country}&output=plot") as u:
+    with urlopen(f"https://excelpythonbase.azurewebsites.net/covidata?country={country}&output=plot") as u:
         return {
             "data": base64.b64encode(u.read()).decode("ascii"),
             "mimeType": u.getheader("Content-Type"),
